e6232bbe03322b07530a36ba0a0440a7daf9842e
joaoGabrielGmr <jggmrodrigues@gmail.com>
2025-11-15 00:17:09 -0300

    update: ironblock

--- ironblock-web/src/app/layout.tsx
+++ ironblock-web/src/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Iron Block - Soluções Inovadoras",
-  description: "Adquira nossas soluções e transforme sua empresa",
+  title: "Iron Block - Tokenização",
+  description: "Tokenize seus ativos e transforme sua empresa",
 };
 
 export default function RootLayout({
